fbf5ff5de8cdb5384bef86b4cb52a0cdcebcc709
aprildaebation <adaecb@gmail.com>
2016-07-04 11:55:20 +0800

    modified models.py using inspectdb and removed simple implementation of user registration (not allauth)

--- web_app/admin.py
+++ web_app/admin.py
@@ -3,6 +3,6 @@ from web_app import models
 
 # Register your models here.
 
-@admin.register(models.ERP_User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('first_name','last_name','email')
+# @admin.register(models.ERP_User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('first_name','last_name','email')
