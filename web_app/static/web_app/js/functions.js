2b5936eb2dcc5ee985073ed080e0c4456bd25cfd
aprildaebation <adaecb@gmail.com>
2016-07-05 10:02:44 +0800

    can import a python script from views

--- web_app/static/web_app/js/functions.js
+++ web_app/static/web_app/js/functions.js
@@ -1,3 +1,3 @@
 function createSite() {
-  alert("Hello World");
+  
 }
--- web_app/views.py
+++ web_app/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from django.views.generic import View
 from web_app.forms import LoginForm
 from django.contrib.auth.models import User
-#from web_app.models import ERP_User
+# from web_app.models import ERP_User
+
+# test_setup is the deploy script for the ERPNext app
+from test_setup import *
+
 
 # Create your views here.
 class HomeView(View):
@@ -13,3 +17,8 @@ class HomeView(View):
 class IndexView(View):
     def get(self,request):
         return render(request,"index.html")
+
+class CreateSite(View):
+    def post(self,request):
+        sayHi()
+        return render(request,"success.html")
