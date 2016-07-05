36bea40ebd1b4fd5d03bc7f140b39601e3eae562
aprildaebation <adaecb@gmail.com>
2016-07-05 16:51:41 +0800

    added shell script which installs prerequisites of django app

---
+++ setup_django_erp.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+get_mariadb(){
+  sudo apt-get update
+  sudo apt-get install python-pip python-dev mariadb-server libmariadbclient-dev libssl-dev
+  sudo mysql_secure_installation
+}
+create_database(){
+  mysql -u root -p -e "CREATE DATABASE erpnext_django CHARACTER SET UTF8;
+  CREATE USER Administrator@localhost IDENTIFIED BY 'password';
+  GRANT ALL PRIVILEGES ON erpnext_django.* TO Administrator@localhost;
+  FLUSH PRIVILEGES;
+  exit"
+}
+
+set_app(){
+  git clone https://github.com/aprildaebation/ERP_Project.git
+  cd ~/ERP_Project
+  pip install -r requirements.txt
+  echo "Done installing requirements"
+}
+
+configure_django_db(){
+  # cd ~/ERP_Project
+  echo "$PWD"
+  echo "Making migrations...."
+  python manage.py makemigrations
+  python manage.py migrate
+}
+
+create_superuser(){
+  # cd ~/erpnext_django
+  echo "Creating superuser...."
+  python manage.py createsuperuser
+}
+
+main(){
+  get_mariadb
+  create_database
+  set_app
+  configure_django_db
+  create_superuser
+  mysql -u Administrator -p erpnext_django < erpnext_django.sql
+}
+main $@
