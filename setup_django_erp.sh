1f2f4224eb901f273ebe9df588c4c4ff38150dc3
aprildaebation <adaecb@gmail.com>
2016-07-06 18:32:50 +0800

    modified shell script to check if mariadb already exists

--- setup_django_erp.sh
+++ setup_django_erp.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
 
 get_mariadb(){
+  echo "Installing MariaDB...."
   sudo apt-get update
   sudo apt-get install python-pip python-dev mariadb-server libmariadbclient-dev libssl-dev
   sudo mysql_secure_installation
 }
 create_database(){
+  echo "Creating database for erpnext_django..."
   mysql -u root -p -e "CREATE DATABASE erpnext_django CHARACTER SET UTF8;
   CREATE USER Administrator@localhost IDENTIFIED BY 'password';
   GRANT ALL PRIVILEGES ON erpnext_django.* TO Administrator@localhost;
@@ -35,7 +41,11 @@ create_superuser(){
 }
 
 main(){
-  get_mariadb
+  if exists mysql; then
+    echo "MySQL already exists"
+  else
+    get_mariadb
+  fi
   create_database
   set_app
   configure_django_db
